7405a7539b039208aac054facb22bf54577e9b40
Bharath Nair <bnair2637dev@gmail.com>
2026-04-28 01:46:13 +0000

    GTDetector class implementation for v1 using GT detections from Nuscenes

--- include/gt_detector.hpp
+++ include/gt_detector.hpp
@@ -1,2 +1,20 @@
 // Concrete Detector: reads ground-truth bounding boxes from a JSON file.
 // Returns detections for a given frame/timestamp from preloaded data.
+
+#include "detector.hpp"
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <string>
+
+using json = nlohmann::json;
+
+/// @brief Concrete Detector that loads ground-truth detections from a JSON file
+class GTDetector : public Detector
+{
+    public:
+    GTDetector(const std::string& json_path);
+    std::vector<Detection> detect(int) override;
+
+    private:
+    json scene_data_;
+};
--- src/gt_detector.cpp
+++ src/gt_detector.cpp
@@ -1,1 +1,49 @@
 // Implementation of GroundTruthDetector: parses JSON, indexes by frame, returns detections.
+
+#include "gt_detector.hpp"
+
+#include <iostream>
+
+GTDetector::GTDetector(const std::string& json_path)
+{
+    std::ifstream file(json_path);
+    scene_data_ = json::parse(file);
+}
+
+std::vector<Detection> GTDetector::detect(int frame_id)
+{
+    std::vector<Detection> detections;
+    json curr_frame_ = scene_data_[frame_id];
+    json curr_detections = curr_frame_["detections"];
+
+    for (int i = 0; i < curr_detections.size(); i++)
+    {
+        Detection detection;
+        detection.instance_token = curr_detections[i]["instance_token"];
+        detection.category_name = curr_detections[i]["category_name"];
+        auto t = curr_detections[i]["translation"];
+        detection.position = Eigen::Vector3d(t[0], t[1], t[2]);
+        auto s = curr_detections[i]["size"];
+        detection.bbox_dims = Eigen::Vector3d(s[0], s[1], s[2]);
+        auto r = curr_detections[i]["rotation"];
+        detection.rotation_quaternion = Eigen::Vector4d(r[0], r[1], r[2], r[3]);
+        detection.yaw = curr_detections[i]["yaw"];
+
+        detections.push_back(detection);
+    }
+
+    return detections;
+}
+
+
+// int main()
+// {
+//     std::unique_ptr<Detector> d = std::make_unique<GTDetector>("../results/gt/scene_0000.json");
+//     std::vector<Detection> detections = d->detect(0);
+//     std::cout<<"**************";
+
+//     for (int i = 0; i < detections.size(); i++)
+//     {
+//         std::cout<<(i+1)<<". "<<detections[i].category_name<<std::endl;
+//     }
+// }
