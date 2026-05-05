3c70cb8259229413565f3d884df0279b5e7a4581
Bharath Nair <bnair2637dev@gmail.com>
2026-05-05 03:08:23 +0000

    comments for clarity

--- src/linear_kf.cpp
+++ src/linear_kf.cpp
@@ -6,22 +6,22 @@
 
 LinearKF::LinearKF(Eigen::Vector3d position)
 {
+    // Tunable Params
     Eigen::VectorXd sigma_squared_state(6);
     sigma_squared_state<< 100, 100, 100, 20, 20, 5; // sigma squared for x, y, z, vx, vy, vz
-
     Eigen::VectorXd sigma_squared_measurement(3);
-    sigma_squared_measurement<< 4, 4, 4; // sigma squared for x, y, z
-
+    sigma_squared_measurement<< 4, 4, 4; // sigma squared for x, y, 
     Eigen::VectorXd sigma_squared_process(6);
     sigma_squared_process << 1, 1, 1, 1, 1, 1;
 
+    // Kalman Filter Matrices Initialization
     x_ = Eigen::VectorXd::Zero(6);
     x_.head(3) = position;
 
     P_ = sigma_squared_state.asDiagonal();
 
     // Partially filling F matrix. 
-    // Only filling in the constant terms, the time dependednt terms will be filled in later
+    // Only filling in the constant terms, the time dependent terms will be filled in later
     F_ = Eigen::MatrixXd::Identity(6, 6);
     
     Q_ = sigma_squared_process.asDiagonal();
