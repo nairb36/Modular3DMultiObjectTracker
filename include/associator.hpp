7bfa558a7c0545719c3e0a4a92cd81fda3969f98
Bharath Nair <bnair2637dev@gmail.com>
2026-05-09 17:08:19 +0000

    hungarain matching done in associator

--- include/associator.hpp
+++ include/associator.hpp
@@ -4,17 +4,22 @@
 
 #include "track.hpp"
 #include "detection.hpp"
+#include "Hungarian.h"
+#include "cost_function.hpp"
 #include <limits>
 
 class Associator
 {
     public:
     Associator() = default;
-    void build_cost_matrix(const std::vector<Track>&, const std::vector<Detection>&);
+    void build_cost_matrix(const std::vector<Track>&, const std::vector<Detection>&, CostFunction&);
+    void perform_bipartite_matching();
 
 
     private:
+    HungarianAlgorithm hungarian_algorithm_;
     std::vector<std::vector<double>> cost_matrix_;
+    std::vector<int> assignments_;
     bool apply_gating_rules(const Track&, const Detection&);
 
 };
--- src/associator.cpp
+++ src/associator.cpp
@@ -3,11 +3,11 @@
 
 #include "associator.hpp"
 
-void Associator::build_cost_matrix(const std::vector<Track>& tracks, const std::vector<Detection>& detections)
+void Associator::build_cost_matrix(const std::vector<Track>& tracks, const std::vector<Detection>& detections, CostFunction& cost_function)
 {
     int curr_num_tracks = tracks.size();
     int curr_num_detections = detections.size();
-    cost_matrix_ = std::vector<std::vector<double>>(curr_num_tracks, 
+    cost_matrix_ = std::vector<std::vector<double>>(curr_num_tracks,
                                                     std::vector<double>(curr_num_detections, std::numeric_limits<double>::infinity()));
 
     for (int i = 0; i < curr_num_tracks; i++)
@@ -16,15 +16,21 @@ void Associator::build_cost_matrix(const std::vector<Track>& tracks, const std::
         {
             if (apply_gating_rules(tracks[i], detections[j]))
             {
-                // Only compute pairwise cost when gating checks have passed, keep at infinity otherwise
-                cost_matrix_[i][j] = cost_function_.compute_cost(tracks[i], detections[j]);
+                cost_matrix_[i][j] = cost_function.compute_cost(tracks[i], detections[j]);
             }
         }
     }
 }
 
-bool Associator::apply_gating_rules()
+
+bool Associator::apply_gating_rules(const Track& track, const Detection& detection)
 {
     // TODO write actual implementation of gating logic
     return true;
+}
+
+
+void Associator::perform_bipartite_matching()
+{
+    double assignment_cost = hungarian_algorithm_.Solve(cost_matrix_, assignments_);
 }
